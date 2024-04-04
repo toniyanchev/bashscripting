adde8f12aa7296f70ea1d7cfa35cf0e2ba59b733
Anton Yanchev <anton.yanchev@a1.bg>
2024-04-04 15:48:13 +0300

    [UPDATE] Add packages

--- ubuntu-pckg-sync/sync.sh
+++ ubuntu-pckg-sync/sync.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 cd $(dirname $0) || exit 1
 
-echo hi from ubuntu-pckg-sync
+git stash
+git pull
+git stash pop
+
+# Save packages to file
+dpkg --get-selections >packages.txt
+
+# Update packages - delete/install
+sudo dpkg --clear-selections
+sudo dpkg --set-selections <packages.txt
+sudo apt-get dselect-upgrade
 
 exit 0
